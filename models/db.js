e7763783555363bf29ac4d918dba8a40259ce620
davidcphan <dphan2@student.unimelb.edu.au>
2018-04-19 02:01:35 +1000

    temporary habit tracker

--- models/db.js
+++ models/db.js
@@ -3,7 +3,9 @@ var faker = require('faker');
 var randomName = faker.name.findName(); // Rowan Nikolaus
 var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 var randomCard = faker.helpers.createCard(); // random contact card containing many properties
-
+var randomStreak = faker.random.number(30);
+var randomGoals = faker.random.number(30);
+var randomPoints= faker.random.number({min:500, max:1000});
 
 
 var userArray = function(){
@@ -15,7 +17,10 @@ var userArray = function(){
       {
       name: faker.name.findName(),
       email: faker.internet.email(),
-      id: i+1
+      id: i+1,
+      streak: faker.random.number(30),
+      goals: faker.random.number(30),
+      points: faker.random.number({min:500, max:1000})
 
       }
    )
