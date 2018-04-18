410c9c06d92ec4879ca116699aabf65227ab519e
alanswf1126 <wsou@student.unimelb.edu.au>
2018-04-19 00:11:49 +1000

    updated

--- Frontend/core.js
+++ Frontend/core.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // JavaScript Document
 
 var d = new Date();
@@ -16,59 +15,25 @@ document.getElementById("today").innerHTML = formatDate(d);
 
 var contentElement = $('#habitRow');
 var addButtonElement = $('#addHabit');
+var categoryOption = $('#category');
 var titleInput = $('#title');
 var repesInput = $('#repes');
 
 var contentTemplate = Handlebars.compile($('#entry-template').text());
 
 function addContent(){
-	var newHabit = contentTemplate({
-		title: titleInput.val(),
-		repes: repesInput.val(),
-	});
-	contentElement.html(newHabit + contentElement.html());
-	//clean up inputs
-	titleInput.val('');
-	repesInput.val('');
-
+	if(categoryOption.val() && titleInput.val() && repesInput.val()){
+		var newHabit = contentTemplate({
+			category: categoryOption.val(),
+			title: titleInput.val(),
+			repes: repesInput.val(),
+		});
+		
+		contentElement.html(newHabit + contentElement.html());
+		//clean up inputs
+		categoryOption.val('');
+		titleInput.val('');
+		repesInput.val('');	
+	};
 }
 addButtonElement.on('click', addContent);
-=======
-// JavaScript Document
-
-var d = new Date();
-function formatDate(date) {
-  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var day = date.getDate();
-  var monthIndex = date.getMonth();
-  var year = date.getFullYear();
-
-  return day + ' ' + monthNames[monthIndex] + ' ' + year;
-}
-document.getElementById("today").innerHTML = formatDate(d);
-
-// Convert HTML form input into JSON object.
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
-
-$(function() {
-    $('form').submit(function() {
-        $('#result').text(JSON.stringify($('form').serializeObject()));
-        return false;
-    });
-});
->>>>>>> e4ced0af3e2ab74bdaf771cb7b4cc7fafd681974
