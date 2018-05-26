93852209e4f448a3a04ad10f4b4499d090d57b6f
davidcphan <dphan2@student.unimelb.edu.au>
2018-05-26 12:10:46 +1000

    db cleanup and background color fixed

--- models/db.js
+++ models/db.js
@@ -22,31 +22,5 @@ mongoose.connect('mongodb://userData:pwd2018@ds117200.mlab.com:17200/user', func
   }
 
 });
-require('./userData.js')
-require('./habitData.js')
-
-var userArray = function(){
-
-  var uArray = [];
-  for(i =0; i < 100; i++){
-
-    uArray.push(
-      {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      id: i+1,
-      streak: faker.random.number(30),
-      goals: faker.random.number(30),
-      points: faker.random.number({min:500, max:1000})
-
-      }
-   )
-
-
-  }
-  return uArray;
-};
-
-var user = userArray();
-
-module.exports = user;
+require('./userData.js');
+require('./habitData.js');
